86a7326afb6c153caf2196bfa453f3940def8814
Nicolas <117557162+MrZLeviatan@users.noreply.github.com>
2023-09-19 04:26:52 -0500

    Estructura Inicial
    Se crea la distribucion de archivos, los indicios de un test y la implementacion de los componentes para el JavaFx

---
+++ SubastasUniquindio/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module co.edu.uniquindio.marketplace {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens co.edu.uniquindio.SubastasUniquindio to javafx.fxml;
+    exports co.edu.uniquindio.SubastasUniquindio;
+}
---
+++ SubastasUniquindio/src/test/java/Usuario.java
@@ -0,0 +1,10 @@
+
+import org.junit.jupiter.api.*;
+
+public class Usuario {
+
+    @Test
+    public void test01 (){
+
+    }
+}
