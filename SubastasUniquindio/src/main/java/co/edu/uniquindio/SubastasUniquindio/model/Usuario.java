3025c2e7c1d09541c1dcac69f217e9db011d8529
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-09-19 15:57:24 -0500

    cree la clase padre Usuario
    donde tiene los atributos generales de un usuario

--- SubastasUniquindio/src/main/java/co/edu/uniquindio/SubastasUniquindio/model/Usuario.java
+++ SubastasUniquindio/src/main/java/co/edu/uniquindio/SubastasUniquindio/model/Usuario.java
@@ -1,4 +1,45 @@
 package co.edu.uniquindio.SubastasUniquindio.model;
 
-public class Usuario {
+import java.time.LocalDate;
+
+public abstract class Usuario {
+    private String nombre;
+    private String cedula;
+    private String correo;
+    private String contraseña;
+    private LocalDate fechaNacimiento;
+    private int edad;
+
+    public Usuario(String nombre, String cedula, String correo, String contraseña, LocalDate fechaNacimiento, int edad) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.fechaNacimiento = fechaNacimiento;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
 }
