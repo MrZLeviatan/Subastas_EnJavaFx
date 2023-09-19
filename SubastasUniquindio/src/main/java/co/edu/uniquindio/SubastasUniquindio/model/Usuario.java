0174923b770f66eb2966f14c1d4176eff70726bf
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-09-19 17:27:15 -0500

    metodo of() a usuario
    con este metodo verifico que los atributos no sean  nulos y se puedan usar para crear la clase  y elemine el abstrap para poderlo usar

--- SubastasUniquindio/src/main/java/co/edu/uniquindio/SubastasUniquindio/model/Usuario.java
+++ SubastasUniquindio/src/main/java/co/edu/uniquindio/SubastasUniquindio/model/Usuario.java
@@ -1,8 +1,10 @@
 package co.edu.uniquindio.SubastasUniquindio.model;
 
+import co.edu.uniquindio.SubastasUniquindio.exceptions.ValorRequeridoException;
 import java.time.LocalDate;
+import java.util.Objects;
 
-public abstract class Usuario {
+public class Usuario {
     private String nombre;
     private String cedula;
     private String correo;
@@ -18,6 +20,30 @@ public abstract class Usuario {
         this.fechaNacimiento = fechaNacimiento;
         this.edad = edad;
     }
+
+    public static Usuario of (String nombre,String cedula, String correo, String contraseña, LocalDate fechaNacimiento, int edad)throws Exception{
+        if(Objects.requireNonNull(nombre,"el nombre es requerido").isBlank()){//verifica si es nulo, que no sean solo espacios
+            throw new ValorRequeridoException("nombre");
+        }
+        if(Objects.requireNonNull(cedula,"el numero de cedula es requerida").isBlank()){
+            throw new ValorRequeridoException("cedula");
+        }
+        if(Objects.requireNonNull(correo,"el correo es necesario").isBlank()){
+            throw new ValorRequeridoException("correo");
+        }
+        if(Objects.requireNonNull(contraseña,"la contraseña es requerida").isBlank()){
+            throw new ValorRequeridoException("contraseña");
+        }
+        if(Objects.requireNonNull(fechaNacimiento,"la fecha de nacimiento es requerida").equals(null)){
+            throw new ValorRequeridoException( "fecha nacimiento");
+        }
+        if(Objects.requireNonNull(edad,"la edad es requerida").equals(null)){
+            throw new ValorRequeridoException("edad");
+        }
+        return new Usuario(nombre, cedula, correo, contraseña, fechaNacimiento, edad);
+    }
+
+
 
     public String getNombre() {
         return nombre;
