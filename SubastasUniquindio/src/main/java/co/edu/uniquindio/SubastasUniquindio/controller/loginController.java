a035e5b6a83043ab0f548ad9860470f769fd7329
Mariana Muñoz Rodriguez <127903568+MarianaRodriguezM@users.noreply.github.com>
2023-09-27 18:27:00 -0500

    interfaz del login.fxml

--- SubastasUniquindio/src/main/java/co/edu/uniquindio/SubastasUniquindio/controller/loginController.java
+++ SubastasUniquindio/src/main/java/co/edu/uniquindio/SubastasUniquindio/controller/loginController.java
@@ -1,15 +1,36 @@
 package co.edu.uniquindio.SubastasUniquindio.controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
 
 public class loginController {
     public DatePicker fechaNacimientoDato;
     public TextField edadDato;
     public TextField correoDato;
     public TextField cedulaDato;
+    public Tab interfazAutenticacion;
+    public TextField idUsuario;
+    public Button ingresarUsuario;
+    public Button ingresarUsuario1;
+    public PasswordField passawordUsuario;
+    public TextField nombreDato;
+    public TableColumn nombreListaAnuncios;
+    public TableColumn codigoListaAnuncios;
+    public TableColumn compradorListaPujas;
+    public TableColumn CodigoListaPujas;
+    public TableColumn fechaListaPujas;
+    public TableView tablaAnuncios;
+    public TableColumn ofertaInicalListaPujas;
+    public TableColumn AnuncianteListaAnuncios;
+    public TableView tablaAnuncios1;
+    public Text textPuja;
+    public TextField pujaRealizadaDato;
+    public TextField codigoPujaDato;
+    public TextField fechaInicioPuja;
+    public TextField ofertaPuja;
+    public Button elegirPujaGanadora;
+    public Text text1;
     @FXML
     private Label welcomeText;
 
