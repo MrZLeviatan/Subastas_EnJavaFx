940ee4c1189b784dbf8a2e203f84e572031b099c
Mariana Muñoz Rodriguez <127903568+MarianaRodriguezM@users.noreply.github.com>
2023-09-27 17:59:31 -0500

    interfaz del login.fxml

--- SubastasUniquindio/src/main/java/co/edu/uniquindio/SubastasUniquindio/controller/loginController.java
+++ SubastasUniquindio/src/main/java/co/edu/uniquindio/SubastasUniquindio/controller/loginController.java
@@ -1,9 +1,15 @@
 package co.edu.uniquindio.SubastasUniquindio.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class loginController {
+    public DatePicker fechaNacimientoDato;
+    public TextField edadDato;
+    public TextField correoDato;
+    public TextField cedulaDato;
     @FXML
     private Label welcomeText;
 
