a03b398c9640e8f296f56b935db9999209f50a0c
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-09-28 00:49:46 -0500

    agrege los elementos los controladores y viewControladores

--- SubastasUniquindio/src/main/java/co/edu/uniquindio/subastasUq/SubastaAplication.java
+++ SubastasUniquindio/src/main/java/co/edu/uniquindio/subastasUq/SubastaAplication.java
@@ -7,12 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SubastaAplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SubastaAplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
