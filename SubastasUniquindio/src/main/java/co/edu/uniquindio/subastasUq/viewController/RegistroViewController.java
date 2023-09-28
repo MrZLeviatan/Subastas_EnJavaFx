0dcacc1a80eb76927390e83df2c6a31dd6c880ef
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-09-28 15:23:07 -0500

    load

--- SubastasUniquindio/src/main/java/co/edu/uniquindio/subastasUq/viewController/RegistroViewController.java
+++ SubastasUniquindio/src/main/java/co/edu/uniquindio/subastasUq/viewController/RegistroViewController.java
@@ -1,6 +1,8 @@
 package co.edu.uniquindio.subastasUq.viewController;
 
 import co.edu.uniquindio.subastasUq.mapping.dto.AnuncianteDto;
+import co.edu.uniquindio.subastasUq.mapping.dto.CompradorDto;
+import co.edu.uniquindio.subastasUq.mapping.dto.UsuarioDto;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
@@ -65,6 +67,64 @@ public class RegistroViewController {
     @FXML
     private TextField txtUsuario;
 
+    private AnuncianteDto construirEmpleadoDto() {
+        return new AnuncianteDto(
+                txtNombre.getText(),
+                "apellido",
+                //txtApellido.getText(),
+                txtCedula.getText(),
+                12,
+                new UsuarioDto(
+                        txtUsuario.getText(),
+                        txtContraseña.getText(),
+                        "correo"
+                )
+        );
+    }
+
+    private CompradorDto construirCompradorDto(){
+        return new CompradorDto(
+                txtNombre.getText(),
+                "apellido",
+                txtCedula.getText(),
+                12,
+                txtDireccion.getText(),
+                new UsuarioDto(
+                        "nombreUsuari0",
+                        txtContraseña.getText(),
+                        txtUsuario.getText()
+                )
+        );
+    }
+
+    private boolean datosValidosCompradorDto(CompradorDto compradorDto){
+        String mensaje= "";
+        if(compradorDto.nombre()==null||compradorDto.nombre().isBlank())
+            mensaje+= "el nombre es invalido \n";
+        if(compradorDto.apellido()==null||compradorDto.apellido().isBlank())
+            mensaje+= "el apellido es invalido \n";
+        if(compradorDto.cedula()==null||compradorDto.cedula().isBlank())
+            mensaje+= "la cedula es invalida \n";
+        if (compradorDto.edad()==null)
+            mensaje+= "la edad es invalida \n";
+        if(compradorDto.direccion()==null||compradorDto.direccion().isBlank())
+            mensaje+= "la direccion es invalida \n";
+        if(compradorDto.usuarioDto()==null)
+            mensaje+= "usuario invalid?? \n";
+        if(compradorDto.usuarioDto().correo()==null)
+            mensaje+= "el correo es invalido\n";
+        if(compradorDto.usuarioDto().contrasena()==null||compradorDto.usuarioDto().contrasena().isBlank())
+            mensaje+= "la contraseña es invalida \n";
+        if(compradorDto.usuarioDto().username()==null|| compradorDto.usuarioDto().username().isBlank())
+            mensaje+= "la contraseña es invalida \n";
+        if(mensaje=="")
+            return true;
+        else {
+            mostrarMensaje("notificacion comprador", "Datos invalidos", mensaje,Alert.AlertType.WARNING);
+            return false;
+        }
+    }
+
     private boolean datosValidosAnuniante(AnuncianteDto anuncianteDto) {
         String mensaje = "";
         if(anuncianteDto.nombre() == null || anuncianteDto.nombre().isBlank())
@@ -77,10 +137,16 @@ public class RegistroViewController {
             mensaje += "La edad es invalida \n" ;
         if(anuncianteDto.usuarioDto()==null)
             mensaje+= "el usario es invalido \n";
+        if(anuncianteDto.usuarioDto().username()==null||anuncianteDto.usuarioDto().username().isBlank())
+            mensaje+= "el nombre de usario es invalido  \n";
+        if(anuncianteDto.usuarioDto().correo()==null||anuncianteDto.usuarioDto().correo().isBlank())
+            mensaje+=" el correo es invalido \n";
+        if(anuncianteDto.usuarioDto().contrasena()==null||anuncianteDto.usuarioDto().contrasena().isBlank())
+            mensaje+= " la contraseña es invalida \n";
         if(mensaje.equals("")){
             return true;
         }else{
-            mostrarMensaje("Notificación cliente","Datos invalidos",mensaje, Alert.AlertType.WARNING);
+            mostrarMensaje("Notificación anunciante","Datos invalidos",mensaje, Alert.AlertType.WARNING);
             return false;
         }
     }
