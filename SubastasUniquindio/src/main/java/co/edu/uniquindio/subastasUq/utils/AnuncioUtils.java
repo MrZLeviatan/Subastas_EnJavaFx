f198a8bba6a995b8d5e7e468995b4cfe4a141723
Andrey3681 <117780962+Andrey3681@users.noreply.github.com>
2023-09-30 04:37:49 -0500

    agrege los utils pa crear los metodos de buscar para la tabla de inicio

--- SubastasUniquindio/src/main/java/co/edu/uniquindio/subastasUq/utils/AnuncioUtils.java
+++ SubastasUniquindio/src/main/java/co/edu/uniquindio/subastasUq/utils/AnuncioUtils.java
@@ -1,4 +1,34 @@
 package co.edu.uniquindio.subastasUq.utils;
 
+import co.edu.uniquindio.subastasUq.model.Anuncio;
+
+import java.util.function.Predicate;
+
 public class AnuncioUtils {
+
+    public static Predicate<Anuncio> buscarPorCodigo(String codigo){
+        return anuncio-> anuncio.getCodigoAnuncio().equals(codigo);
+    }
+
+    public static Predicate<Anuncio> buscarPorAnunciante(String nombreAnunciante){
+        return anuncio -> anuncio.getNombreAnunciante().equals(nombreAnunciante);
+    }
+
+    public static Predicate<Anuncio> buscarPorNombreProducto(String nombreProducto){
+        return anuncio -> anuncio.getProducto().getNombre().equals(nombreProducto);
+    }
+
+    public static Predicate<Anuncio> buscarPorTodo(String codigo,String nombreAnunciante,String nombreProducto) {
+        Predicate<Anuncio> predicado = anuncio -> true;
+        if( codigo != null && !codigo.isBlank() ){
+            predicado = predicado.and(buscarPorCodigo(codigo));
+        }
+        if( nombreAnunciante != null && !nombreAnunciante.isBlank() ){
+            predicado = predicado.and(buscarPorAnunciante(nombreAnunciante));
+        }
+        if( nombreProducto != null && !nombreProducto.isBlank() ){
+            predicado = predicado.and(buscarPorNombreProducto(nombreProducto));
+        }
+        return predicado;
+    }
 }
